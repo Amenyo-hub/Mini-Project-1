d78b64daca634533e81d727d4390a9999d907b66
Amenyo-hub <amenyoe63@gmail.com>
2025-09-02 21:12:57 +0000

    Add files via upload

---
+++ Library Book Category Checker.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+char category;
+
+  
+    printf("Enter the first letter of the book category (F/N/S/H): ");
+    scanf(" %c", &category);
+
+    
+    switch(category) {
+        case 'F':
+        case 'f':
+            printf("Fiction\n");
+            break;
+        case 'N':
+        case 'n':
+            printf("Non-Fiction\n");
+            break;
+        case 'S':
+        case 's':
+            printf("Science\n");
+            break;
+        case 'H':
+        case 'h':
+            printf("History\n");
+            break;
+        default:
+            printf("Invalid category entered!\n");
+    }	
+	return 0;
+}
